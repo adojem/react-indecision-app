5a5f816ee8c06053aa5b394fe3dc845244110b8b
adojem <adojemcovix@gmail.com>
2018-08-10 21:57:14 +0900

    Indecision State

---
+++ src/playground/build-it-visibile.js
@@ -0,0 +1,25 @@
+const visibility = false;
+
+const toggleVisibility = () => {
+   visibility = !visibility;
+   render();
+};
+
+const render = () => {
+   const jsx = (
+      <div>
+         <h1>Visibility Toggle</h1>
+         <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'Show details'}</button>
+         {visibility && (
+            <div>
+               <p>Hey. These are some details you can now see!</p>
+            </div>
+         )}
+      </div>
+   );
+
+   const jsx = (
+   ReactDOM.render(jsx, document.getElementById('app'));
+};
+
+render();
