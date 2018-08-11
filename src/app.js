0a374b78b17bf379c986096006ac2f5ac98bf938
adojem <adojemcovix@gmail.com>
2018-08-11 14:49:41 +0900

    Saving and Loading Options Data

--- src/app.js
+++ src/app.js
@@ -12,6 +12,31 @@ class IndecisionApp extends React.Component {
       this.handleDeleteOption = this.handleDeleteOption.bind(this);
    }
 
+   componentDidMount() {
+      try {
+         const json = localStorage.getItem('options');
+         const options = JSON.parse(json);
+
+         if (options) {
+            this.setState(() => ({ options }));
+         }
+      }
+      catch (e) {
+         // Do nothing at all
+      }
+   }
+
+   componentDidUpdate(prevProps, prevState) {
+      if (prevState.options.length !== this.state.options.length) {
+         const json = JSON.stringify(this.state.options);
+         localStorage.setItem('options', json);
+      } 
+   }
+
+   componentWillUnmount() {
+      console.log('componentWillUnmount');
+   }
+
    handleDeleteOptions() {
       this.setState(() => ({ options: [] }));
    }
@@ -98,6 +123,7 @@ const Options = (props) => {
    return (
       <div>
          <button onClick={props.handleDeleteOptions}>Remove All</button>
+         {props.options.length === 0 && <p>Please add an option to get started!</p>}
          {props.options.map(option => (
             <Option
                key={option}
@@ -141,6 +167,10 @@ class AddOption extends React.Component {
       const error = this.props.handleAddOption(option);
 
       this.setState(() => ({ error }));
+
+      if (!error) {
+         e.target.elements.option.value = '';
+      }
    }
 
    render() {
