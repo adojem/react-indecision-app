047243312960d5f449b2c93036c640a2d645a76d
adojem <adojemcovix@gmail.com>
2018-08-11 11:05:59 +0900

    Default Prop Values

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ class IndecisionApp extends React.Component {
       super(props);
 
       this.state = {
-         options: []
+         options: props.options
       };
 
       this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
@@ -41,12 +41,11 @@ class IndecisionApp extends React.Component {
    }
 
    render() {
-      const title = 'Indecision';
       const subtitle = 'Put your life in the hands of a computer';
 
       return (
          <div>
-            <Header title={title} subtitle={subtitle} />
+            <Header subtitle={subtitle} />
             <Action
                hasOptions={this.state.options.length > 0}
                handlePick={this.handlePick}
@@ -63,14 +62,22 @@ class IndecisionApp extends React.Component {
    }
 }
 
+IndecisionApp.defaultProps = {
+   options: []
+};
+
 const Header = (props) => {
    return (
       <div>
          <h1>{props.title}</h1>
-         <h2>{props.subtitle}</h2>
+         {props.subtitle && <h2>{props.subtitle}</h2>}
       </div>
    );
-}
+};
+
+Header.defaultProps = {
+   title: 'Indecision'
+};
 
 const Action = (props) => {
    return (
